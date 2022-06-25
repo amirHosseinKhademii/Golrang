ec730590b6b0c31587b3f1bf5cb1bbfdb085b40c
amirHosseinKhademii <sourena2575@gmail.com>
2022-06-25 11:50:57 +0200

    Move the joiner to utils folder

--- src/components/span/Span.tsx
+++ src/components/span/Span.tsx
@@ -1,24 +1,24 @@
 import { ComponentProps } from "react";
-
+import { joiner } from "utils";
 
 export type TSpan = {
   classType: "stitle" | "sprice";
 } & ComponentProps<"span">;
 
-
-const joiner = (...classes: any) => classes.filter(Boolean).join(" ");
-
-export const Span = ({ children,classType }: TSpan) => {
+export const Span = ({ children, classType }: TSpan) => {
   return (
     <>
-    <span 
-    className={joiner(
-      "block",
-      classType === "stitle" ? "opacity-75 -mb-1" :
-      //  "font-semibold text-xl" :
-       "bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center"
-    )}
-    >{children}</span>
+      <span
+        className={joiner(
+          "block",
+          classType === "stitle"
+            ? "opacity-75 -mb-1"
+            : //  "font-semibold text-xl" :
+              "bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center"
+        )}
+      >
+        {children}
+      </span>
     </>
   );
 };
